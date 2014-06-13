2878d4cd1885b745a0868ce8822ef027abec0a58
Robert <robert_zibert@hotmail.com>
2014-06-13 15:14:07 -0400

    agragegando tabla

---
+++ app/database/migrations/2014_06_13_181914_create_diagnosis_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateDiagnosisTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('diagnosis', function(Blueprint $table)
+		{
+			$table->increments('id');
+			//Foreign Keys
+			$table->integer('appointments_id')->unsigned();
+			//diagnosis attr
+			$table->text('observations');
+			$table->timestamps();
+
+
+		});
+			//Foreign Keys
+		Schema::table('diagnosis', function($table)
+		{	
+    	$table->foreign('appointments_id')->references('id')->on('appointments');
+		});
+	}	
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('diagnosis');
+	}
+
+}
