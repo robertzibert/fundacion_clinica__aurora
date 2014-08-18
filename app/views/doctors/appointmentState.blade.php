b172554b4477c0a6f72c8a11a7d2d7ae0119b54a
ggarreton <german.garreton@gmail.com>
2014-08-17 20:28:55 -0400

    Cambios eddit appointment doctores, solo cambiar entre done y canceled y puesto en español

--- app/views/doctors/appointmentState.blade.php
+++ app/views/doctors/appointmentState.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 <!--app/views/doctors/appointmentState.blade.php-->
 @section('content')
-<h1>Edit a Appointment between Dr. {{$appointment->doctor->user->name}} and patient {{$appointment->patient->user->name}}</h1>
+<h1>Editar estado consulta entre el Dr. {{$appointment->doctor->user->name}} y el paciente {{$appointment->patient->user->name}}</h1>
 
 <!-- if there are creation errors, they will show here -->
 {{ HTML::ul($errors->all()) }}
@@ -9,11 +9,11 @@
 {{ Form::model($appointment, array('action' => array('DoctorsController@updateAppointment',$appointment->id))) }}
 
 	<div class="form-group">
-		{{ Form::label('price', 'Price') }}
-		{{ Form::select('state', array('canceled' => 'canceled', 'reserved' => 'reserved', 'confirmed' => 'confirmed'), 'reserved') }}
+		{{ Form::label('price', 'Estado') }}
+		{{ Form::select('state', array('canceled' => 'Cancelada', 'done' => 'Completada'), 'Cancelada', array('class' => 'form-control')) }}
 	</div>
 
-	{{ Form::submit('Edit the appointment', array('class' => 'btn btn-primary')) }}
+	{{ Form::submit('Confirmar Edición', array('class' => 'btn btn-primary')) }}
 
 {{ Form::close() }}
 
