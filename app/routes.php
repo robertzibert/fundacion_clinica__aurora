6ab53cb6a2a6e8ee93680cc81d72deb8c2020639
Robert <robert_zibert@hotmail.com>
2014-09-10 16:07:50 -0300

    routeando todo y arreglando defectos en el logout

--- app/routes.php
+++ app/routes.php
@@ -10,15 +10,18 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+
+Route::get('/', 'LoginController@index');
 Route::post('doctors/updateAppointment/{id}', 'DoctorsController@updateAppointment');
 Route::get('appointments/{appointment_id}/editappointment', 'DoctorsController@editAppointment');
 Route::get('admins/history','AdminsController@history');
 Route::get('logout','LoginController@destroy');
-Route::get('/', 'LoginController@index');
 Route::resource('login', 'LoginController');
 Route::resource('admins', 'AdminsController');
 Route::resource('doctors', 'DoctorsController');
 
+
+//Routes to create Appointments
 Route::get('appointments/step/1', ['as' => 'appointments.create.step_1', 'uses' => 'AppointmentsController@step_1']);
 Route::post('appointments/step/2',['as' => 'appointments.create.step_2', 'uses' => 'AppointmentsController@step_2']);
 Route::post('appointments/step/3',['as' => 'appointments.create.step_3', 'uses' => 'AppointmentsController@step_3']);
