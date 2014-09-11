20f243f86d2a322ea5cbc2345293360463a2ca9e
Robert <robert_zibert@hotmail.com>
2014-09-11 00:05:00 -0300

    ahora se pueden anular consultas

--- app/routes.php
+++ app/routes.php
@@ -11,11 +11,11 @@
 |
 */
 
-Route::get('/', 'LoginController@index');
 Route::post('doctors/updateAppointment/{id}', 'DoctorsController@updateAppointment');
 Route::get('appointments/{appointment_id}/editappointment', 'DoctorsController@editAppointment');
 Route::get('admins/history','AdminsController@history');
 Route::get('logout','LoginController@destroy');
+Route::get('/', 'LoginController@index');
 Route::resource('login', 'LoginController');
 Route::resource('admins', 'AdminsController');
 Route::resource('doctors', 'DoctorsController');
@@ -25,6 +25,9 @@ Route::resource('doctors', 'DoctorsController');
 Route::get('appointments/step/1', ['as' => 'appointments.create.step_1', 'uses' => 'AppointmentsController@step_1']);
 Route::post('appointments/step/2',['as' => 'appointments.create.step_2', 'uses' => 'AppointmentsController@step_2']);
 Route::post('appointments/step/3',['as' => 'appointments.create.step_3', 'uses' => 'AppointmentsController@step_3']);
+
+//Route to void an Appointment
+Route::get ('appointments/void/{id}',['as' => 'appointments.void', 'uses' => 'AppointmentsController@void']);
 
 
 Route::resource('appointments', 'AppointmentsController');
