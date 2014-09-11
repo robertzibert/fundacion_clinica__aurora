5c34aabb1b4d651e9c81e45fb3627bde0e70e9f0
Robert <robert_zibert@hotmail.com>
2014-09-11 00:21:56 -0300

    panel de doctores se vé mejor

--- app/views/doctors/show.blade.php
+++ app/views/doctors/show.blade.php
@@ -1,40 +1,40 @@
 @extends('layouts.master')
 <!--app/views/doctors/show.blade.php-->
 @section('content')
-<h1>Dr. {{$doctor->user->name}} {{$doctor->user->lastname}}</h1>
-<p>Universidad: {{$doctor->university}}</p>
-<p>Telefono {{$doctor->phone}}</p>
-<p>Celular: {{$doctor->cellphone}}</p>
-<h2>Consultas</h2>
-
-<table class = "table table-striped table-bordered">
-	<thead>
-			<th>Id</th>
-			<th>Nombre Paciente</th>
-			<th>Apellido Paciente</th>
-			<th>Email Paciente</th>
-			<th>Fecha</th>
-			<th>Precio Consulta</th>
-			<th>Estado de la Consulta</th>
-			<th>Actions</th>
-	</thead>
-	<tbody>
-	@foreach($appointments as $appointment)
-		<tr>
-			<td>{{ $appointment->id }}</td>
-			<td>{{ $appointment->patient->user->name }}</td>
-			<td>{{ $appointment->patient->user->lastname }}</td>
-			<td>{{ $appointment->patient->user->email }}</td>
-			<td>{{ $appointment->active_at }}</td>
-			<td>{{ $appointment->price }}</td>
-			<td>{{ $appointment->state}}</td>
-			<!-- we will also add show, edit, and delete buttons -->
-			<td>
-				<a class="btn btn-small btn-info" href="{{ URL::to('appointments/' . $appointment->id . '/editappointment') }}">Edit</a>
-
-			</td>
-		</tr>
-	@endforeach
-	</tbody>
-</table>
+<div class="row">
+	<div class="well col-md-12">
+		<div class = "col-md-3">
+			<h1>Dr. {{$doctor->user->name}} {{$doctor->user->lastname}}</h1>
+			<p>Universidad: {{$doctor->university}}</p>
+			<p>Telefono {{$doctor->phone}}</p>
+			<p>Celular: {{$doctor->cellphone}}</p>
+			</div>
+		<div class = "col-md-offset-1 col-md-8">
+			<table class = "table table-striped table-bordered">
+				<thead>
+						<th>Nombre Paciente</th>
+						<th>Apellido Paciente</th>
+						<th>Fecha</th>
+						<th>Estado de la Consulta</th>
+						<th>Acciones</th>
+				</thead>
+				<tbody>
+				@foreach($appointments as $appointment)
+					<tr>
+						<td>{{ $appointment->patient->user->name }}</td>
+						<td>{{ $appointment->patient->user->lastname }}</td>
+						<td>{{ $appointment->active_at }}</td>
+						<td>{{ $appointment->state}}</td>
+						<!-- we will also add show, edit, and delete buttons -->
+						<td>
+							<a class="btn btn-small btn-info" href="{{ URL::to('appointments/' . $appointment->id . '/editappointment')}">Editar</a>
+						</td>
+					</tr>
+				@endforeach
+				</tbody>
+			</table>
+			
+		</div>
+	</div>
+</div>
 @stop
